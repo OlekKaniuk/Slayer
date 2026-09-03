63434883ac1663115838929b8744a6cd30c5fe9e
OlekKaniuk <oleksandrkaniuka994@gmail.com>
2026-09-03 22:51:25 +0300

    added long-lights on cards when they are talking

--- script.js
+++ script.js
@@ -7,14 +7,22 @@ cards.forEach(card => {
         const soundPath = card.getAttribute('data-sound');
 
         if (soundPath) {
+            cards.forEach(c => c.classList.remove('is-speaking'));
+            audioPlayer.onended = null;
+
             audioPlayer.pause();
             audioPlayer.currentTime = 0;
 
+            card.classList.add('is-speaking');
             audioPlayer.src = soundPath;
             audioPlayer.play();
+
+            audioPlayer.onended = () => {
+                card.classList.remove('is-speaking');
+            };
         }
-    })
-})
+    });
+});
 
 // Показуємо кнопку, коли користувач прокрутив вниз на 300px
 window.addEventListener('scroll', () => {
